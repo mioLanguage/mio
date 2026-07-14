d0882413c6b53f7ff89588d058e1822b4f546fba
HZY1618yzh <13326945856@sina.cn>
2026-07-14 16:22:38 +0800

    fix win-arm

--- src/build.ps1
+++ src/build.ps1
@@ -69,7 +69,9 @@ $clangArgs = @(
 # On ARM64, disable linker optimizations to avoid "misaligned ldr/str offset" bug
 if ($env:PROCESSOR_ARCHITECTURE -eq "ARM64") {
     Write-Host "ARM64: adding /OPT:NOLBR,NOICF,NOREF to work around lld-link alignment bug"
-    $clangArgs += "-Wl,/OPT:NOLBR,NOICF,NOREF"
+    $clangArgs += "-Wl,/OPT:NOLBR"
+    $clangArgs += "-Wl,/OPT:NOICF"
+    $clangArgs += "-Wl,/OPT:NOREF"
 }
 $clangArgs += ($libs | ForEach-Object { "-l$_" })
 $clangArgs += @(
